a73dced9486d8aa8cf0910d6f5e5cb580484ed8d
JonhGab <148014933+JonhGab@users.noreply.github.com>
2025-02-17 21:23:26 -0300

    Add files via upload

---
+++ Entities/Magazine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOActivity.Entities
+{
+    class Magazine : libraryItems   
+    {
+        public string Edition { get; set; }
+        
+        public Magazine(string title, string edition)
+        {
+            Title = title;
+            Edition = edition;
+        }
+        public override void ExibirDetalhes()
+        {
+            Console.WriteLine($"[Livro] Título: {Title}, Edição: {Edition}, Disponível: {Availability}");
+        }
+    }
+
+}
